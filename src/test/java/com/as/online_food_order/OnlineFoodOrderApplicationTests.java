8fdb8801c01e6bb240ecb62079e7f01fb2c88cd4
Abhay singh <abhaysinghjprk6@gmail.com>
2024-08-29 22:52:01 +0530

    first commit

---
+++ src/test/java/com/as/online_food_order/OnlineFoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.online_food_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
