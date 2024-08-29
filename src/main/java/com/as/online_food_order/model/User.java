373746d3b1d53aecdec9809dd77ccd9f927383cf
Abhay singh <abhaysinghjprk6@gmail.com>
2024-08-29 22:55:00 +0530

    "second commit"

---
+++ src/main/java/com/as/online_food_order/model/User.java
@@ -0,0 +1,14 @@
+package com.as.online_food_order.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+}
